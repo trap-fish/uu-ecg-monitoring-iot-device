3e32bcd94a32ac3900970630397b7737784e614e
trap-fish <fishfoshfish@outlook.com>
2024-01-06 17:43:30 +0000

    update the dash app to read the actual signal from DB for the last 20 seconds, removed test files

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,49 +1,59 @@
 import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
+from dash import Dash, dcc, html, callback, Input, Output
+
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import pandas as pd
 import plotly.express as px
+import time
 
-# Initialize Dash app
-app = dash.Dash(__name__)
+# define the AWS DynamoDB table object
+ecg_dev_tab = boto3.resource('dynamodb', region_name='eu-west-1')
+table = ecg_dev_tab.Table('ecg_dev')
 
-# AWS DynamoDB Configuration
-dynamodb = boto3.resource('dynamodb', region_name='region-region-1')
-table_name = '<table_name'
-table = dynamodb.Table(table_name)
+# initialize the dash app
+app = Dash(__name__)
 
-# Layout of the app
+# define app layout
 app.layout = html.Div([
-    html.H1("DynamoDB Graph"),
-    dcc.Graph(id='graph'),
+    html.H2('ECG Livefeed'),
+    html.Div(id='live-update-graph'),
+    dcc.Graph(id='live-update-waveform'),
     dcc.Interval(
         id='interval-component',
-        interval=1 * 1000,  # in milliseconds
+        interval = 5000, # ms
         n_intervals=0
-    ),
+    )
 ])
 
-# Callback to update the graph
-@app.callback(Output('graph', 'figure'), [Input('interval-component', 'n_intervals')])
-def update_graph(n):
-    # Query data from DynamoDB
-    response = table.scan()
-    items = response['Items']
+# callback for auto-updates every interval (ms), s defined in layout
+@callback(Output('live-update-waveform', 'figure'),
+          Input('interval-component', 'n_intervals'))
 
-    # Convert DynamoDB items to DataFrame
+def update_waveform(n):
+
+    # get the timeframes to scan data
+    ts_now = int(time.time()) * 1000
+    prev_ts = int(ts_now - 20000)
+    
+    # scan dynamoDB table and extract items
+    date_filter = Key('measurement_time').between(prev_ts, ts_now)
+    ecg_data = table.scan(FilterExpression=date_filter)
+    items = ecg_data['Items']
+
+    # create a datafranme and convert to correct data types
     df = pd.DataFrame(items)
+    df['measurement'] = pd.to_numeric(df['measurement'])
+    df['measurement_id'] = pd.to_numeric(df['measurement_id'], downcast='unsigned')
+    df['measurement_time'] = pd.to_numeric(df['measurement_time'], downcast='unsigned')
+    df['measurement_time'] = pd.to_datetime(df['measurement_time'], unit='ms')
+    # Sort DataFrame by 'measure_id'
+    df = df.sort_values(by='measurement_id')
 
-    # Convert 'measurement' column to numeric type and handle NaN values
-    df['measurement'] = pd.to_numeric(df['measurement'], errors='coerce')
-
-    # Drop NaN values for 'measurement'
-    df = df.dropna(subset=['measurement'])
-
-    # Create a line graph using Plotly Express
-    fig = px.line(df, x='measurement_time', y='measurement', labels={'measurement_time': 'Measurement Time', 'measurement': 'Measurement'},
+    fig = px.line(df, x= 'measurement_time', y='measurement',  
+                  labels={'measure_id': 'Measurement Time', 'measurement': 'Measurement'},
                   title='DynamoDB Data Visualization')
-
+    
     return fig
 
 if __name__ == '__main__':
